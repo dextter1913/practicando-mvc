993683f066886613cd575e9d4d37d08ea532e37d
dextter1913 <dextter1913@gmail.com>
2021-06-08 11:18:40 -0500

    Agregando autoload al mvc

--- controller/empleadocontroller.php
+++ controller/empleadocontroller.php
@@ -7,6 +7,13 @@
             $mostrarView = $mostrar->mostrarDatos();
             require_once 'view/mostrardatos.php';
         }
+        public function IngresandoDatos(){
+            require_once 'view/ingresardatos.php';
+            if (isset($_GET['btnmostrar'])) {
+                require_once 'model/empleado.php';
+                $mostrar = new empleado($_GET['id'],$_GET['nombre'],$_GET['apellido']);
+                $mostrarView = $mostrar->MostrarDatos();
+                require_once 'view/mostrardatos.php';
+            }
+        }
     }
-    
-?>
--- view/ingresardatos.php
+++ view/ingresardatos.php
@@ -2,13 +2,14 @@
     <div class="row">
         <div class="col-12 col-sm-6 col-md-4 col-lg-4"></div>
         <div class="col-12 col-sm-6 col-md-4 col-lg-4">
-            <form action="model/empleado.php" method="post">
+            <form action="?ingresar" method="get">
                 <label for="id">ID</label>
                 <input type="text" name="id" id="id" class="form-control">
                 <label for="nombre">Nombre</label>
                 <input type="text" name="nombre" id="nombre" class="form-control">
                 <label for="apellido">Apellido</label>
                 <input type="text" name="apellido" id="apellido" class="form-control">
+                <input type="submit" value="ingresar" name="btnmostrar" class="btn btn-outline-danger btn-sm">
             </form>
         </div>
         <div class="col-12 col-sm-6 col-md-4 col-lg-4"></div>
